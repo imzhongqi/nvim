8f64a1e71c7eefe6c3d109c99e2b2ad4705ee2fa
zhongqi <imzhongqi@gmail.com>
2024-01-12 23:58:26 +0800

    update scroll

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -11,8 +11,8 @@ return {
       set_highlights = true,
       folds = 1000, -- handle folds, set to number to disable folds if no. of lines in buffer exceeds this
       max_lines = false, -- disables if no. of lines in buffer exceeds this
-      hide_if_all_visible = false, -- Hides everything if all lines are visible
-      throttle_ms = 100,
+      hide_if_all_visible = true, -- Hides everything if all lines are visible
+      throttle_ms = 1000,
       excluded_buftypes = {
         "terminal",
       },
@@ -34,6 +34,99 @@ return {
         handle = true,
         search = false, -- Requires hlslens
         ale = false, -- Requires ALE
+      },
+
+      marks = {
+        Cursor = {
+          text = "-",
+          priority = 0,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Normal",
+        },
+        Search = {
+          text = { "-", "=" },
+          priority = 1,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Search",
+        },
+        Error = {
+          text = { "-", "=" },
+          priority = 2,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextError",
+        },
+        Warn = {
+          text = { "-", "=" },
+          priority = 3,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextWarn",
+        },
+        Info = {
+          text = { "-", "=" },
+          priority = 4,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextInfo",
+        },
+        Hint = {
+          text = { "-", "=" },
+          priority = 5,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "DiagnosticVirtualTextHint",
+        },
+        Misc = {
+          text = { "-", "=" },
+          priority = 6,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "Normal",
+        },
+        GitAdd = {
+          text = "┆",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsAdd",
+        },
+        GitChange = {
+          text = "┆",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsChange",
+        },
+        GitDelete = {
+          text = "▁",
+          priority = 7,
+          gui = nil,
+          color = nil,
+          cterm = nil,
+          color_nr = nil, -- cterm
+          highlight = "GitSignsDelete",
+        },
       },
     },
   },
