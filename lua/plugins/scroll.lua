1d9a130cab084d17118b64596a537e568b884ccf
zhongqi <imzhongqi@gmail.com>
2024-01-14 18:23:34 +0800

    update scroll

--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -12,8 +12,8 @@ return {
       -- Syntax: t[keys] = {function, {function arguments}}
       mappings["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "120" } }
       mappings["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "120" } }
-      mappings["<C-b>"] = { "scroll", { "-vim.fn.winheight(0)", "true", "150" } }
-      mappings["<C-f>"] = { "scroll", { "vim.fn.winheight(0)", "true", "150" } }
+      mappings["<C-b>"] = { "scroll", { "-vim.fn.winheight(0)", "true", "160" } }
+      mappings["<C-f>"] = { "scroll", { "vim.fn.winheight(0)", "true", "160" } }
       mappings["<C-y>"] = { "scroll", { "-0.10", "false", "50" } }
       mappings["<C-e>"] = { "scroll", { "0.10", "false", "50" } }
       mappings["zt"] = { "zt", { "250" } }
