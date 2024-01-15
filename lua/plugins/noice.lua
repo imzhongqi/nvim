39bd0b51dac03a049ba3651b1d576d3309d0fc90
zhongqi <imzhongqi@gmail.com>
2024-01-15 21:29:10 +0800

    update

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -83,4 +83,45 @@ return {
       progress = {},
     },
   },
+
+  keys = function()
+    return {
+      {
+        "<S-Enter>",
+        function()
+          require("noice").redirect(vim.fn.getcmdline())
+        end,
+        mode = "c",
+        desc = "Redirect Cmdline",
+      },
+      {
+        "<leader>snl",
+        function()
+          require("noice").cmd("last")
+        end,
+        desc = "Noice Last Message",
+      },
+      {
+        "<leader>snh",
+        function()
+          require("noice").cmd("history")
+        end,
+        desc = "Noice History",
+      },
+      {
+        "<leader>sna",
+        function()
+          require("noice").cmd("all")
+        end,
+        desc = "Noice All",
+      },
+      {
+        "<leader>snd",
+        function()
+          require("noice").cmd("dismiss")
+        end,
+        desc = "Dismiss All",
+      },
+    }
+  end,
 }
--- lua/plugins/scroll.lua
+++ lua/plugins/scroll.lua
@@ -4,6 +4,7 @@ return {
     event = "VeryLazy",
     opts = {
       -- hide_cursor = false,
+      easing_function = "quadratic",
     },
     config = function(_, opts)
       require("neoscroll").setup(opts)
@@ -12,8 +13,8 @@ return {
       -- Syntax: t[keys] = {function, {function arguments}}
       mappings["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "100" } }
       mappings["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
-      mappings["<C-b>"] = { "scroll", { "-vim.fn.winheight(0)", "true", "180" } }
-      mappings["<C-f>"] = { "scroll", { "vim.fn.winheight(0)", "true", "180" } }
+      mappings["<C-b>"] = { "scroll", { "-vim.fn.winheight(0)", "true", "120" } }
+      mappings["<C-f>"] = { "scroll", { "vim.fn.winheight(0)", "true", "120" } }
       mappings["<C-y>"] = { "scroll", { "-0.10", "false", "50" } }
       mappings["<C-e>"] = { "scroll", { "0.10", "false", "50" } }
       mappings["zt"] = { "zt", { "250" } }
