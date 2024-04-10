41135e99b4aa6296c052a44610b9385d95bb952c
zhongqi <imzhongqi@gmail.com>
2024-04-10 18:29:52 +0800

    fix fundo

--- lua/plugins/tool/undo.lua
+++ lua/plugins/tool/undo.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "kevinhwang91/nvim-fundo",
+    lazy = false, -- note: this plugin is not lazy loaded
+    opts = {},
+    build = function() require("fundo").install() end,
+  },
+
+  {
     "mbbill/undotree",
     cmd = {
       "UndotreeToggle",
@@ -25,19 +32,11 @@ return {
       DiffAutoOpen = 0,
       HighlightChangedWithSign = 0,
       TreeNodeShape = "",
-      UndoDir = vim.fn.stdpath "cache" .. "/undodir",
     },
     config = function(_, opts)
       for k, v in pairs(opts) do
         vim.g["undotree_" .. k] = v
       end
     end,
-  },
-
-  {
-    "kevinhwang91/nvim-fundo",
-    event = "VeryLazy",
-    opts = {},
-    build = function() require("fundo").install() end,
   },
 }
